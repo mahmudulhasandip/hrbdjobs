41bf989adbb24e85b529da4d9f5d4930b3b51ff9
Mahmudul Hasan Dip <ablaze.dip@gmail.com>
2018-09-10 18:00:59 +0600

    update hot job details

--- resources/views/users/load.blade.php
+++ resources/views/users/load.blade.php
@@ -4,7 +4,8 @@
 		@php
 			$single_job = route('single.job', $job->id);
 		@endphp
-		<div class="job-listing wtabs">
+
+		<div class="job-listing wtabs {{ ($job->is_featured) ? 'featured' : '' }}">
 			<div class="job-title-sec pointer newtab" onclick="viewJob('{{ $single_job}}')">
 				<div class="c-logo"> <img src="{{ asset('storage/uploads/'.(($job->employer->employerCompanyInfo['logo']) ? $job->employer->employerCompanyInfo['logo'] : 'default_user.png'))}}" alt="" /> </div>
 				<h3><a href="#" title="">{{ $job->title }}</a></h3>
@@ -15,7 +16,7 @@
 			<div class="job-style-bx">
 				<span class="job-is ft">Full time</span>
 				@if($job->vacancy)
-				<span class="mt-5 job-is ft">Vacancy - {{ $job->vacancy }}</span>
+				<span class="mt-5 vacancy">Vacancy: {{ $job->vacancy }}</span>
 				@endif
 				<i><i class="la la-clock-o"></i> {{ date("M d, Y", strtotime($job->deadline)) }}</i>
 			</div>
